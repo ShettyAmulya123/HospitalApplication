2f0c136b6aa4e4e026d97ce8a0be6a453147f1e4
ShettyAmulya123 <amulyashetty203@gmail.com>
2025-06-17 13:29:52 +0530

    Swagger_Completion

--- .history/src/main/java/com/app/hospital/Config/SecurityConfig_20250616195820.java
+++ .history/src/main/java/com/app/hospital/Config/SecurityConfig_20250616195820.java
@@ -1,17 +1,17 @@
 package com.app.hospital.Config;
 
+import com.app.hospital.Services.UserDetailsServiceImpl;
+import com.app.hospital.Config.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.app.hospital.Services.UserDetailsServiceImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
